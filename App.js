c2cd3593971c9f8ddf67651f4d18b825459631e8
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-05-21 16:39:39 -0400

    Add yargs

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 const validator = require("validator")
 const chalk = require("chalk")
-
+const yargs = require("yargs")
 const getNotes = require("./Notes.js")
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
 
-console.log(validator.isEmail("alex@email.com"))
+// if(command === "add") {
+//      console.log("Adding note")
+// } else if (command === "remove") {
+//      console.log("Removing note")
+// }
 
-console.log(chalk.green("Success"))
-
-
-console.log(process.argv[2])
+// console.log(process.argv)
 // provides array containing node info and path to the file and any additional info added with script
+
+console.log(yargs.argv)
+
