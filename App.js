03bf0c6bd120fe9c439be9c331d9aea44e98cd30
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-05-21 16:10:56 -0400

    Add chalk package

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+const validator = require("validator")
+const chalk = require("chalk")
+
 const getNotes = require("./Notes.js")
 
 const msg = getNotes()
-
 console.log(msg)
+
+console.log(validator.isEmail("alex@email.com"))
+
+console.log(chalk.green("Success"))
