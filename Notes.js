d2fbbb19c09c0c7cdb95af104ee0face84a37691
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-05-24 15:31:02 -0400

    Refactor notes file with arrow functions

--- Notes.js
+++ Notes.js
@@ -1,45 +1,42 @@
 const fs = require("fs");
-const chalk = require("chalk")
+const chalk = require("chalk");
 
-const getNotes = function () {
+const getNotes = () => {
   return "Your notes ...";
 };
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter((note => note.title === title))
-     if(!duplicateNotes.length){
+  const duplicateNotes = notes.filter((note) => note.title === title);
+  if (!duplicateNotes.length) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+  } else {
+    console.log("Note title taken");
+  }
+};
 
-  notes.push({
-    title: title,
-    body: body,
-  });
-  saveNotes(notes);
-
-} else {
-     console.log("Note title taken")
-}
-}
-
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
   const data = JSON.stringify(notes);
   fs.writeFileSync("notes.json", data);
 };
 
-const removeNote = function (title) {
-     const notes = loadNotes();
-     const notesToKeep = notes.filter(note => note.title !== title)
-     
-     if(notes.length > notesToKeep.length) {
-          console.log(chalk.green.inverse("Note removed"))
-          saveNotes(notesToKeep)
-     } else {
-          console.log(chalk.red.inverse("No note found"))
-     }
-    
-}
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note) => note.title !== title);
 
-const loadNotes = function () {
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("No note found"));
+  }
+};
+
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
     const dataJson = dataBuffer.toString();
@@ -52,5 +49,5 @@ const loadNotes = function () {
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
-  removeNote: removeNote
+  removeNote: removeNote,
 };
